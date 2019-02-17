5138e61c0b92fa7eb38d2a78f7fe42af5dfccd35
Mike Keohane <mkeohane@gatech.edu>
2019-02-17 15:24:56 -0500

    added skills array to player class

--- Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Player.java
+++ Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Player.java
@@ -5,11 +5,13 @@ public class Player {
     private int credits;
     private Ships ship;
     private int skillPoints;
+    private Skills[] playerSkills;
 
     public Player(String name, int credits, Ships ship) {
         this.name = name;
         this.credits = credits;
         this.ship = ship;
+        playerSkills = Skills.values();
     }
 
     public int getCredits() {
