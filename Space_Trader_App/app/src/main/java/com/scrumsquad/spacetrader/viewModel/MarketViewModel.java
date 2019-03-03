e7b1ff8b457168c29b1204d787f350460f349a8c
Matt Carroll <mwcarroll12@gmail.com>
2019-03-03 14:37:59 -0500

    MarketViewModel added

---
+++ Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/viewModel/MarketViewModel.java
@@ -0,0 +1,19 @@
+package com.scrumsquad.spacetrader.viewModel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+
+import com.scrumsquad.spacetrader.model.Difficulty;
+import com.scrumsquad.spacetrader.model.Game;
+import com.scrumsquad.spacetrader.model.Player;
+import com.scrumsquad.spacetrader.model.Ships;
+import com.scrumsquad.spacetrader.model.Skills;
+
+import java.sql.SQLOutput;
+
+public class MarketViewModel extends ViewModel {
+
+
+}
