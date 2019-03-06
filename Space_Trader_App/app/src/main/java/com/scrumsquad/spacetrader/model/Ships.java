e56db6314bb779706e70654900c46d7320fbae24
CooperLink <Cooper.r.link@gmail.com>
2019-03-06 14:57:14 -0500

    Added Flyweight for Ships
    Added the flyweight

--- Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Ships.java
+++ Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Ships.java
@@ -1,55 +1,46 @@
 package com.scrumsquad.spacetrader.model;
 
+
+// This should be treated essentially as a resource for the ship class
 public enum Ships {
-    GNAT  ("Gnat", 15);
+    GNAT  (15);
 
-    private final String name;
-    private final MarketGoodItem[] inventory;
 
-    Ships(final String name, final int capacity) {
-        this.inventory = new MarketGoodItem[capacity];
-        this.name = name;
+    //These allow us to build the ship.
+    // They should stay inside of this ships class
+    // Only use them to instantiate
+    private int cargoCap;
+    private int hullStrength;
+    private int weaponCap;
+    private int shieldCap;
+    private int gadgetCap;
+    private int crewCap;
+    private int range;
+
+
+
+    Ships(int cargoCap) {
+        this.cargoCap = cargoCap;
+
     }
 
-    public String getName() { return name; }
+    //Rewritten name method
+    public String toString() {
+        String name = this.name();
+        name = name.toLowerCase();
+        name = name.substring(0, 1).toUpperCase() + name.substring(1);
+        return name;
 
-    public MarketGoodItem[] getInventory() {
-        return inventory;
     }
 
-    public boolean canAdd() {
-        for (int i = 0; i <= inventory.length - 1; i++) {
-            if (inventory[i] == null) {
-                return true;
-            }
-        }
-        return false;
+    // These will be getters for each characteristic
+    public int getCargoCap() {
+        return cargoCap;
     }
 
-    public void addItem(MarketGoodItem bought) {
-        if(canAdd()) {
-            // This is O(2N) rn which isn't great
-            for (int i = 0; i <= inventory.length - 1; i++) {
-                if (inventory[i] == null) {
-                    inventory[i] = bought;
-                    System.out.println("Added " + bought.name() + " to ship inventory");
-                    return;
-                }
-            }
-        }
-    }
 
-    public MarketGoodItem removeItem(MarketGoodItem sold) {
-        for (int i = 0; i <= inventory.length - 1; i++) {
-            if (sold.equals(inventory[i])) {
-                MarketGoodItem soldItem = inventory[i];
-                inventory[i] = null;
-                System.out.println("Removed " + soldItem.name() + " from ship inventory");
-                return soldItem;
-            }
-        }
-        // Item is not in inventory
-        System.out.println(sold.name() + " is not in ship inventory");
-        return null;
-    }
+
+
+
+
 }
