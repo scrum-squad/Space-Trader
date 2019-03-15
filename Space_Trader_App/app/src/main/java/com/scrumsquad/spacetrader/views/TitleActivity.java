5db449a6de6f64e7c1062944b1d583eeeb1090b6
Aviva Kern <aviva.ashley@gmail.com>
2019-03-15 18:54:21 -0400

    Added title screen artwork

--- Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/views/TitleActivity.java
+++ Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/views/TitleActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.scrumsquad.spacetrader.R;
 import com.scrumsquad.spacetrader.views.ConfigurationActivity;
@@ -16,7 +17,7 @@ public class TitleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        Button startBtn = findViewById(R.id.title_start_player_config);
+        ImageButton startBtn = findViewById(R.id.title_start_player_config);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
