5a2b051bbbcd4f74fd696ac08dc6ccd36828e319
Jackson Williams <jwilliams01619@gmail.com>
2019-04-07 15:28:25 -0400

    Unit Tests and Revision to Ship Logic
    Created a JUnit test for the Ship class's removeItem method
    
    Changed Ship add logic to use ArrayList logic instead of sudo array logic
    Possible TODO: Change logic into full array logic due to cargo capacity limits

--- Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Ship.java
+++ Space_Trader_App/app/src/main/java/com/scrumsquad/spacetrader/model/Ship.java
@@ -9,19 +9,24 @@ public class Ship {
     private List<MarketGoodItem> inventory;
     private int fuel;
     private final int MAX_FUEL;
+    private int itemCap;
 
     public Ship() {
         Ships type = Ships.GNAT;
-        MarketGoodItem[] list = new MarketGoodItem[type.getCargoCap()];
-        this.inventory = new ArrayList(Arrays.asList(list));
+//        MarketGoodItem[] list = new MarketGoodItem[type.getCargoCap()];
+//        this.inventory = new ArrayList(Arrays.asList(list));
+        this.inventory = new ArrayList<>();
+        this.itemCap = type.getCargoCap();
         this.name = type.toString();
         this.fuel = type.getFuelCap();
         this.MAX_FUEL = type.getFuelCap();
     }
 
     public Ship(Ships type) {
-        MarketGoodItem[] list = new MarketGoodItem[type.getCargoCap()];
-        this.inventory = new ArrayList(Arrays.asList(list));
+//        MarketGoodItem[] list = new MarketGoodItem[type.getCargoCap()];
+//        this.inventory = new ArrayList(Arrays.asList(list));
+        this.inventory = new ArrayList<>();
+        this.itemCap = type.getCargoCap();
         this.name = type.toString();
         this.fuel = type.getFuelCap();
         this.MAX_FUEL = type.getFuelCap();
@@ -32,40 +37,52 @@ public class Ship {
     }
 
     public boolean canAdd() {
-        System.out.println("major: " + inventory.size());
-        for (int i = 0; i <= inventory.size() - 1; i++) {
-            System.out.println("key: " + inventory.get(i));
-            if (inventory.get(i) == null) {
-                return true;
-            }
-        }
-        return false;
+//        System.out.println("major: " + inventory.size());
+//        for (int i = 0; i <= inventory.size() - 1; i++) {
+//            System.out.println("key: " + inventory.get(i));
+//            if (inventory.get(i) == null) {
+//                return true;
+//            }
+//        }
+//        return false;
+        return this.inventory.size() < this.itemCap;
+
     }
 
     public void addItem(MarketGoodItem bought) {
         if(canAdd()) {
-            // This is O(2N) rn which isn't great
-            for (int i = 0; i <= inventory.size() - 1; i++) {
-                if (inventory.get(i) == null) {
-                    inventory.set(i, bought);
-                    System.out.println("Added " + bought.name() + " to ship inventory");
-                    return;
-                }
-            }
+//            // This is O(2N) rn which isn't great
+//            for (int i = 0; i <= inventory.size() - 1; i++) {
+//                if (inventory.get(i) == null) {
+//                    inventory.set(i, bought);
+//                    System.out.println("Added " + bought.name() + " to ship inventory");
+//                    return;
+//                }
+//            }
+
+            this.inventory.add(bought);
         }
     }
 
     public MarketGoodItem removeItem(MarketGoodItem sold) {
-        for (int i = 0; i <= inventory.size() - 1; i++) {
+//        for (int i = 0; i <= inventory.size() - 1; i++) {
+//            if (sold.equals(inventory.get(i))) {
+//                MarketGoodItem soldItem = inventory.get(i);
+//                inventory.set(i, null);
+//                System.out.println("Removed " + soldItem.name() + " from ship inventory");
+//                return soldItem;
+//            }
+//        }
+//        // Item is not in inventory
+//        System.out.println(sold.name() + " is not in ship inventory");
+//        return null;
+
+        for (int i = 0; i < inventory.size(); i++) {
             if (sold.equals(inventory.get(i))) {
-                MarketGoodItem soldItem = inventory.get(i);
-                inventory.set(i, null);
-                System.out.println("Removed " + soldItem.name() + " from ship inventory");
-                return soldItem;
+                return inventory.remove(i);
             }
         }
-        // Item is not in inventory
-        System.out.println(sold.name() + " is not in ship inventory");
+
         return null;
     }
 
